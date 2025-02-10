5bcf5a4e7e6447dd128fefa09dd7f13dd747ba29
SánchezCarlos <carlossanllon@gmail.com>
2025-02-10 13:32:02 +0100

    Refactor main.dart to simplify the app structure and replace NewsList with a static 'Hola Mundo' message

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
@@ -13,54 +11,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Noticias'),
+        body: const Center(
+          child: Text('Hola Mundo'),
         ),
-        body: const NewsList(),
       ),
     );
-  }
-}
-
-class NewsList extends StatefulWidget {
-  const NewsList({super.key});
-
-  @override
-  _NewsListState createState() => _NewsListState();
-}
-
-class _NewsListState extends State<NewsList> {
-  List _news = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchNews();
-  }
-
-  Future<void> fetchNews() async {
-    final response = await http.get(Uri.parse('https://newsapi.org/v2/top-headlines?country=us&apiKey=e81b37ac736f468abfd8c7a7aa8fc378'));
-    if (response.statusCode == 200) {
-      setState(() {
-        _news = json.decode(response.body)['articles'];
-      });
-    } else {
-      throw Exception('Failed to load news');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _news.isEmpty
-        ? const Center(child: CircularProgressIndicator())
-        : ListView.builder(
-            itemCount: _news.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_news[index]['title']),
-                subtitle: Text(_news[index]['description']),
-              );
-            },
-          );
   }
 }
