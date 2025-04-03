9a5f7f2d62fdf30c5e4b1f3f7318b81e3b57d182
Sanchez Pellon <cspellon@indra.es>
2025-04-03 17:42:45 +0200

    Agregar pantallas de inicio, registro y perfil; actualizar rutas en main.dart

---
+++ flutter_application_1/lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: Text('Bienvenido a la pantalla de inicio!'),
+      ),
+    );
+  }
+}
