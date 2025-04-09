38b9ae681bdb7565977e14b12cf11318326d0a54
migueldam2 <miguel.estudiossr@gmail.com>
2025-04-09 13:22:36 +0200

    actualizacion del script de bbdd

--- ficheros/bbdd.sql
+++ ficheros/bbdd.sql
@@ -1,11 +1,15 @@
--- Creación de la base de datos (opcional)
+-- Creación de la base de datos
+\c postgres
+DROP DATABASE agora;
+
 CREATE DATABASE agora;
 \c agora; 
--- Conectar a la base de datos (solo en PostgreSQL)
+
 
 -- Creación de la tabla nation
 CREATE TABLE nation (
-    nationName VARCHAR(100) UNIQUE NOT NULL PRIMARY KEY
+    code INT PRIMARY KEY,
+    nation_name VARCHAR (100) UNIQUE NOT NULL
 );
 
 -- Creación de la tabla user
@@ -14,9 +18,9 @@ CREATE TABLE users (
     username VARCHAR(100) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
-    nation VARCHAR(100) NOT NULL,
+    nation INT NOT NULL,
     admin BOOLEAN DEFAULT false,
-    FOREIGN KEY (nation) REFERENCES nation(nationName) ON DELETE SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (nation) REFERENCES nation(code) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- Creación de la tabla following
@@ -31,16 +35,16 @@ CREATE TABLE following (
 
 -- Creación de la tabla topic
 CREATE TABLE topic (
-    topicName VARCHAR(100) UNIQUE NOT NULL PRIMARY KEY
+    topic_name VARCHAR(100) UNIQUE NOT NULL PRIMARY KEY
 );
 
 -- Creación de la tabla user_topic
 CREATE TABLE user_topic (
     id_user INT,
-    topicName VARCHAR(100),
-    PRIMARY KEY (id_user, topicName),
+    topic_name VARCHAR(100),
+    PRIMARY KEY (id_user, topic_name),
     FOREIGN KEY (id_user) REFERENCES users(id_user) ON DELETE CASCADE,
-    FOREIGN KEY (topicName) REFERENCES topic(topicName) ON DELETE CASCADE
+    FOREIGN KEY (topic_name) REFERENCES topic(topic_name) ON DELETE CASCADE
 );
 
 -- Creación de la tabla post
@@ -48,14 +52,14 @@ CREATE TABLE post (
     id_post SERIAL PRIMARY KEY,
     id_user INT NOT NULL,
     title VARCHAR(100),
-    nation VARCHAR(100) NOT NULL,
+    nation INT NOT NULL,
     topic VARCHAR(100),
     content TEXT NOT NULL,
     post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     parent_post INT NULL,
     FOREIGN KEY (id_user) REFERENCES users(id_user) ON DELETE CASCADE,
-    FOREIGN KEY (nation) REFERENCES nation(nationName) ON DELETE SET NULL,
-    FOREIGN KEY (topic) REFERENCES topic(topicName) ON DELETE SET NULL,
+    FOREIGN KEY (nation) REFERENCES nation(code) ON DELETE SET NULL,
+    FOREIGN KEY (topic) REFERENCES topic(topic_name) ON DELETE SET NULL,
     FOREIGN KEY (parent_post) REFERENCES post(id_post) ON DELETE CASCADE
 );
 
@@ -67,5 +71,6 @@ CREATE TABLE noticia (
     content TEXT NOT NULL
 );
 
--- Insert de una nacion para el registro
-INSERT INTO nation (nationName) VALUES ('Grecia');
+-- Insert de las naciones
+INSERT INTO nation (code, nation_name) VALUES 
+(34, 'Spain');
