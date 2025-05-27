e35e696f56d4287194e63627da7ffc2a58943e86
CarlosSPellon <166994756+CarlosSPellon@users.noreply.github.com>
2025-05-27 19:11:15 +0200

    Modificar la duración del token JWT a 12 horas en el inicio de sesión

--- app/backend/controllers/userController.js
+++ app/backend/controllers/userController.js
@@ -85,7 +85,7 @@ const loginUser = async (req, res) => {
     const token = jwt.sign(
       { id_user: user.id_user, username: user.username, email: user.email, nation: user.nation },
       process.env.JWT_SECRET || 'clave_secreta',
-      { expiresIn: '24h' }
+      { expiresIn: '12h' }
     );
 
     res.status(200).json({ message: 'Inicio de sesión exitoso', token, user: userData });
