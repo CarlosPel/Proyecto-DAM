443b68dcb59544eba2a6ec1de5b8209b3f8b1fd5
migueldam2 <miguel.estudiossr@gmail.com>
2025-05-29 10:57:36 +0200

    Actualizacion getPost user

--- app/backend/controllers/userController.js
+++ app/backend/controllers/userController.js
@@ -173,10 +173,10 @@ const editProfileUser = async (req, res) => {
 const userPosts = async (req, res) => {
   const id_user = req.user.id_user;
   console.log(id_user);
-  const query = `SELECT * FROM post WHERE id_user = $1 AND parent_post = null ORDER BY post_date ASC`;
+  const query = `SELECT * FROM post WHERE id_user = $1 AND parent_post is null ORDER BY post_date ASC;`;
   try {
     const resultado = await pool.query(query, [id_user]);
-    console.log(resultado.rows[0])
+    console.log(resultado.rows[0].idPost)
     res.status(200).json({
       message: 'Posts extraídos correctamente',
       data: resultado.rows,
@@ -189,7 +189,7 @@ const userPosts = async (req, res) => {
 
 const userConditions = async (req, res) => {
   const id_user = req.user.id_user;
-  const query = 'UPDATE users SET has_agreed = TRUE WHERE id_user = $1';
+  const query = 'UPDATE users SET has_agreed = TRUE WHERE id_user = $1;';
   try {
     const result = await pool.query(query, [id_user]);
     res.status(200).json({
