b1416d1bd208f1cf918627e50d3ebd7c63eadbd3
migueldam2 <miguel.estudiossr@gmail.com>
2025-05-29 11:00:59 +0200

    Actualizacion getPost user2

--- app/backend/controllers/userController.js
+++ app/backend/controllers/userController.js
@@ -173,7 +173,7 @@ const editProfileUser = async (req, res) => {
 const userPosts = async (req, res) => {
   const id_user = req.user.id_user;
   console.log(id_user);
-  const query = `SELECT * FROM post WHERE id_user = $1 AND parent_post is null ORDER BY post_date ASC;`;
+  const query = `SELECT * FROM post WHERE id_user = $1 AND parent_post is null ORDER BY post_date DESC;`;
   try {
     const resultado = await pool.query(query, [id_user]);
     console.log(resultado.rows[0].idPost)
