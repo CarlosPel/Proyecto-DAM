22d80c48efab708182e833b9428b4ff85e298f48
Miguel <miguel.estudiossr@gmail.com>
2025-06-05 11:52:10 +0200

    Añade ruta para desmarcar un post como guardado en el enrutador de publicaciones

--- app/backend/routes/postRoutes.js
+++ app/backend/routes/postRoutes.js
@@ -1,7 +1,7 @@
 //º Importar funciones y frameworks/librerías.
 const express = require('express');
 const { authenticateUser } = require('../middlewares/auth.js');
-const { createPost, getPost, getComments, getFollowedPosts, getParentPost, savePost, checkSaved } = require('../controllers/postController.js');
+const { createPost, getPost, getComments, getFollowedPosts, getParentPost, savePost, checkSaved, unSavePost } = require('../controllers/postController.js');
 
 // Creamos un enrutador para manejar rutas de manera modular, fuera del fichero server.js
 const router = express.Router();
@@ -14,6 +14,7 @@ router.post('/getfollowed', authenticateUser, getFollowedPosts);
 router.post('/parentpost', getParentPost);
 router.post('/save', authenticateUser, savePost);
 router.post('/checksaved', authenticateUser, checkSaved);
+router.post('/unsave', authenticateUser, unSavePost);
 
 // Exportamos el enrutador para ser utilizado en otros ficheros.
 module.exports = router;
